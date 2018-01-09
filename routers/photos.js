23a912b345e1d0fca9d1e37061af08f46b394591
Steven Li <steven4354@gmail.com>
2018-01-09 14:58:43 -0800

    photo upload complete

--- routers/photos.js
+++ routers/photos.js
@@ -1,11 +1,27 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.get('/', (req, res) => {
-  res.render('upload');
+//for getting the aws services package
+const FileUpload = require("../services/file_upload");
+
+router.get("/", (req, res) => {
+  res.render("upload");
 });
 
-router.post('/', (req, res) => {
-  console.log(req.body);
+const mw = FileUpload.single("photo[file]");
+router.post("/", mw, (req, res, next) => {
+  console.log("Files", req.file);
+
+  FileUpload.upload({
+    data: req.file.buffer,
+    name: req.file.originalname,
+    mimetype: req.file.mimetype
+  })
+    .then(data => {
+      console.log("data =>", data);
+      req.flash("success", "Photo created!");
+      res.redirect("/photos");
+    })
+    .catch(next);
 });
 
 module.exports = router;
